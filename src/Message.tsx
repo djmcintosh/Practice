e12878e110d6c910558f50791cae849ee79513c5
devoremcintosh <devoremcintosh@gmail.com>
2023-03-27 00:43:21 -0400

    Practice

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+// PascalCasing
+function Message () {
+    // JSX: JavaScript Code
+    const name = 'Devore';
+    if (name)
+     return <h1>Hello {name}</h1>
+     // can also use getName
+  return <h1>Hello World {name}</h1>;
+}
+
+export default Message;
